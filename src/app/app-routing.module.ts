cd03689c12d303203076bfdc77714b9ed0b04248
Rodrigo Nunes <nunesrodrigo13@outlook.com>
2019-01-15 22:22:26 -0200

    [AppRouting] Set route to dashboard - Adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
